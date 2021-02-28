b26ed424409d936c944ff84d50af2ec1282e6780
VuThiDiu <56347983+VuThiDiu@users.noreply.github.com>
2021-03-01 01:08:19 +0700

    Authentication on Android using PhoneNumber

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:first_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -7,6 +9,9 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  String phoneNumber = "+84528801147";
+  String verificationCode;
+  String smsCode;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,7 +51,9 @@ class _RegisterPageState extends State<RegisterPage> {
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.all(Radius.circular(20)),
                             ),
-                            onPressed: (){},
+                            onPressed: (){
+                              _verifyPhone();
+                            },
                             child: Text("REGISTER",
                               style: TextStyle(
                                 fontSize: 20,
@@ -98,4 +105,92 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
     );
   }
+  Future <void> _verifyPhone() async {
+    final PhoneVerificationCompleted verificationCompleted = (AuthCredential credential){
+      setState(() {
+        print("Verification");
+        print(credential);
+      });
+    };
+
+    final PhoneVerificationFailed phoneVerificationFailed = (AuthException exception){
+      print("${exception.message}");
+    };
+
+    final PhoneCodeSent phoneCodeSent = (String verId, [int forceCodeResend]){
+      this.verificationCode = verId;
+      smsCodeDialog(context).then((value)=>
+      {
+        print("Signed In")
+      }
+      );
+    };
+
+    final PhoneCodeAutoRetrievalTimeout autoRetrievalTimeout = (String verId){
+      this.verificationCode = verId;
+    };
+
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: this.phoneNumber, 
+        timeout: const Duration(seconds: 5), 
+        verificationCompleted: verificationCompleted, 
+        verificationFailed: phoneVerificationFailed,
+        codeSent: phoneCodeSent,
+        codeAutoRetrievalTimeout: autoRetrievalTimeout);
+  }
+
+  Future<bool> smsCodeDialog(BuildContext context){
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text("Enter Code",
+          style: TextStyle(
+            color: Colors.green
+          ),),
+          content: TextField(
+            onChanged: (val){
+              smsCode = val;
+            },
+          ),
+          contentPadding: EdgeInsets.all(10),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Verify",
+                style: TextStyle(
+                  color: Colors.green
+                ),),
+                onPressed: (){
+                  FirebaseAuth.instance.currentUser().then((user){
+                    if(user!=null){
+                      Navigator.of(context).pop();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => loginPage())
+                      );
+                    }else{
+                      Navigator.of(context).pop();
+                      signIn();
+                      //print('no');
+                    }
+                  });
+                },
+              )
+            ],
+
+        );
+      }
+    );
+  }
+
+  signIn(){
+    AuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(verificationId: verificationCode, smsCode: smsCode);
+    FirebaseAuth.instance.signInWithCredential(phoneAuthCredential).then((user)=> Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => loginPage()),
+    )).catchError((e)=>print(e));
+  }
+
 }
